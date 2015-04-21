25dcc1d77bc34306901ac3240742ab2c6a8f7d55
I. Lucut <ioan.lucut88@gmail.com>
2015-04-21 18:37:00 +0200

    refactor(expenses)  - category dropdown - made it more beautiful

--- src/app/expenses/filter/expenses-header-filter.js
+++ src/app/expenses/filter/expenses-header-filter.js
@@ -2,7 +2,7 @@ angular
     .module("expenses")
     .filter('expensesHeader', function ($sce) {
         return function (text, reverse) {
-            var template = reverse ? '<a class="expense-list-box__header__past" href="#">You have $1 expenses</a>' : '<span class="expense-list-box__header__upcoming">Your expenses</span>';
+            var template = reverse ? '<span class="expense-list-box__header__past">You have $1 expenses</span>' : '<span class="expense-list-box__header__upcoming">Your expenses</span>';
 
             return $sce.trustAsHtml(template.replace('$1', text || '0'))
         };
