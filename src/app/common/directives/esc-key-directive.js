b7204f76706f31e26b6ce1fef80366963cd71410
I. Lucut <ioan.lucut88@gmail.com>
2015-04-23 21:47:08 +0200

    feat() - nice.

---
+++ src/app/common/directives/esc-key-directive.js
@@ -0,0 +1,15 @@
+angular
+    .module("common")
+    .directive('escKey', function () {
+        return function (scope, element, attrs) {
+            element.bind('keydown keypress', function (event) {
+                if ( event.which === 27 ) { // 27 = esc key
+                    scope.$apply(function () {
+                        scope.$eval(attrs.escKey);
+                    });
+
+                    event.preventDefault();
+                }
+            });
+        };
+    });
