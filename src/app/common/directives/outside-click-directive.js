cf8106b21f377014efba41940616523345ccb004
I. Lucut <ioan.lucut88@gmail.com>
2015-04-24 00:44:57 +0200

    feat() - fix  a bug where clicks on the autocomplete made the eidt form disappear.

--- src/app/common/directives/outside-click-directive.js
+++ src/app/common/directives/outside-click-directive.js
@@ -6,6 +6,11 @@ angular
             link: function (scope, elem, attrs) {
 
                 $document.on("click", function (event) {
+
+                    /* please ignore clicks from angucomplete */
+                    if ( event.target.className.indexOf("angucomplete") > -1 ) {
+                        return;
+                    }
                     var isChild = elem.find(event.target).length > 0;
 
                     if ( !isChild ) {
