2ae1c2eb0fc8430fd2ab162238569999c385ebf9
I. Lucut <ioan.lucut88@gmail.com>
2015-05-05 21:39:39 +0200

    refactor(settings) - few improvements

--- src/app/settings/controllers/settings-update-password-controller.js
+++ src/app/settings/controllers/settings-update-password-controller.js
@@ -3,7 +3,7 @@
  */
 angular
     .module("settings")
-    .controller("SettingsUpdatePasswordController", function ($scope, flash, AuthService, ACCOUNT_FORM_STATE, ALERTS_CONSTANTS) {
+    .controller("SettingsUpdatePasswordController", function ($scope, flash, $timeout, AuthService, ACCOUNT_FORM_STATE, ALERTS_CONSTANTS) {
 
         var TIMEOUT_PENDING = 300;
 
@@ -29,30 +29,37 @@ angular
          * @param updatePasswordData
          */
         $scope.updatePassword = function (updatePasswordData) {
-            if ( $scope.updatePasswordForm.$valid && !$scope.isRequestPending ) {
-
-                $scope.isRequestPending = true;
-
-                AuthService
-                    .updatePassword(updatePasswordData.oldPassword, updatePasswordData.newPassword, updatePasswordData.newPasswordConfirmation)
-                    .then(function () {
-                        flash.to($scope.alertIdentifierId).success = 'We\'ve successfully updated your account!';
-
-                        $timeout(function () {
-                            $scope.isRequestPending = false;
-                        }, TIMEOUT_PENDING);
-                    })
-                    .catch(function () {
-                        /* If bad feedback from server */
-                        $scope.badPostSubmitResponse = true;
-                        $scope.isRequestPending = false;
-
-                        flash.to($scope.alertIdentifierId).error = 'We\'re not able to update your account. Please try again.';
-                    })
-                    .finally(function () {
-                        $scope.updatePasswordForm.$setPristine();
-                        $scope.updatePasswordData = angular.copy(initialUpdatePasswordData);
-                    });
+            if ( !( $scope.updatePasswordForm.$valid && !$scope.isRequestPending ) ) {
+                return;
             }
+
+            if ( updatePasswordData.newPassword !== updatePasswordData.newPasswordConfirmation ) {
+                flash.to($scope.alertIdentifierId).error = 'Your new password should match the new confirmation password!';
+
+                return;
+            }
+
+            $scope.isRequestPending = true;
+
+            AuthService
+                .updatePassword(updatePasswordData.oldPassword, updatePasswordData.newPassword, updatePasswordData.newPasswordConfirmation)
+                .then(function () {
+                    flash.to($scope.alertIdentifierId).success = 'We\'ve successfully updated your account!';
+
+                    $timeout(function () {
+                        $scope.isRequestPending = false;
+                    }, TIMEOUT_PENDING);
+                })
+                .catch(function () {
+                    /* If bad feedback from server */
+                    $scope.badPostSubmitResponse = true;
+                    $scope.isRequestPending = false;
+
+                    flash.to($scope.alertIdentifierId).error = 'We\'re not able to update your account. Please try again.';
+                })
+                .finally(function () {
+                    $scope.updatePasswordForm.$setPristine();
+                    $scope.updatePasswordData = angular.copy(initialUpdatePasswordData);
+                });
         };
     });
