e1eba1aa497c7e80817f45b9742294236e7020a2
I. Lucut <ioan.lucut88@gmail.com>
2015-05-10 22:19:21 +0200

    feat(import) - updated import controller

--- src/app/import/controllers/expenses-import-controller.js
+++ src/app/import/controllers/expenses-import-controller.js
@@ -79,47 +79,38 @@ angular
             }
         });
 
+        // ---
+        // If something went wrong, show an error message.
+        // ---
         uploader.onWhenAddingFileFailed = function (item, filter, options) {
 
-            flash.to($scope.alertIdentifierId).error = 'We\'ve encountered an error while trying to uploade this shit.'
+            flash.to($scope.alertIdentifierId).error = 'We\'ve encountered an error while trying to upload your expenses.'
         };
-        uploader.onAfterAddingFile = function (fileItem) {
-            console.info('onAfterAddingFile', fileItem);
-        };
-        uploader.onAfterAddingAll = function (addedFileItems) {
-            console.info('onAfterAddingAll', addedFileItems);
-        };
-        uploader.onBeforeUploadItem = function (item) {
-            console.info('onBeforeUploadItem', item);
-        };
-        uploader.onProgressItem = function (fileItem, progress) {
-            console.info('onProgressItem', fileItem, progress);
-        };
-        uploader.onProgressAll = function (progress) {
-            console.info('onProgressAll', progress);
-        };
+
+        // ---
+        // If successful, take the answer.
+        // ---
         uploader.onSuccessItem = function (fileItem, response, status, headers) {
 
             $scope.expensesImportAnswer = ExpensesImport.build(response);
         };
-        uploader.onErrorItem = function (fileItem, response, status, headers) {
-            console.info('onErrorItem', fileItem, response, status, headers);
-        };
-        uploader.onCancelItem = function (fileItem, response, status, headers) {
-            console.info('onCancelItem', fileItem, response, status, headers);
-        };
-        uploader.onCompleteItem = function (fileItem, response, status, headers) {
-            console.info('onCompleteItem', fileItem, response, status, headers);
 
-            // ---
-            // Mark upload completed.
-            // ---
+        // ---
+        // On error item.
+        // ---
+        uploader.onErrorItem = function (fileItem, response, status, headers) {
+
+            flash.to($scope.alertIdentifierId).error = 'We\'ve encountered an error while trying to parse your expenses.'
+        };
+
+        // ---
+        // Mark upload completed.
+        // ---
+        uploader.onCompleteItem = function (fileItem, response, status, headers) {
+
             $timeout(function () {
                 $scope.isUploadFinished = true;
             }, TIMEOUT_PENDING);
-        };
-        uploader.onCompleteAll = function () {
-            console.info('onCompleteAll');
         };
 
         /**
