387ce4dd3fe1a77ab0d9efb307c194c29cf14c0b
I. Lucut <ioan.lucut88@gmail.com>
2015-06-07 02:35:59 +0300

    refactor(tests) - Add few more tests

--- src/app/account/services/interceptors/auth-filter-service.js
+++ src/app/account/services/interceptors/auth-filter-service.js
@@ -10,13 +10,11 @@ angular
                 (toState.url === '/account'
                 || toState.name === "home")
                 && AuthService.isAuthenticated() ) {
-                console.log('1');
 
                 /*If user is authenticated, and tries to go to /account or home, just to expenses*/
                 event.preventDefault();
                 StatesHandler.goToExpenses();
             } else if ( !AuthService.isAuthenticated() && !toState.isPublicPage ) {
-                console.log('2');
 
                 /*If user is not authenticated, save attempt try and go to /account, where login modal is opened*/
                 event.preventDefault();
@@ -26,7 +24,6 @@ angular
                 toState.url.indexOf("/setup") > -1
                 && AuthService.isAuthenticated()
                 && User.$new().loadFromSession().isInitiated() ) {
-                console.log('3');
 
                 /*Once user is initiated, do not let user to setup page*/
                 event.preventDefault();
@@ -36,7 +33,6 @@ angular
                 && toState.url.indexOf("/setup") === -1
                 && AuthService.isAuthenticated()
                 && !User.$new().loadFromSession().isInitiated() ) {
-                console.log('4');
 
                 /*If user is not initiated but authenticated, and tries to go to a non public page, go to setup page*/
                 event.preventDefault();
@@ -46,7 +42,6 @@ angular
                 && !toState.isPaymentRelatedPage
                 && AuthService.isAuthenticated()
                 && User.$new().loadFromSession().isTrialPeriodExpired() ) {
-                console.log('5');
 
                 /*If user is with trial expired, authenticated and tries to go to a non public page, go to payment*/
                 event.preventDefault();
---
+++ src/app/account/services/interceptors/auth-filter=service_test.js
@@ -0,0 +1,148 @@
+describe('app/AuthFilter', function () {
+
+    var $rootScope, $location, $state, $injector, $httpBackend, $q, ENV, AccountModal, CURRENCY_URLS, COLOR_URLS, EXPENSE_URLS, CATEGORY_URLS, STATES, AuthServiceMock, UserMock;
+
+    beforeEach(function () {
+
+        // ---
+        // Just inject the module and define dependencies.
+        // ---
+        module('app', function ($provide) {
+            $provide.value('AuthService', AuthServiceMock = {});
+            $provide.value('User', UserMock = {});
+        });
+
+        inject(function (_$rootScope_, _$state_, _$location_, _$injector_, _$httpBackend_, _$q_, _ENV_, _AccountModal_, _CURRENCY_URLS_, _COLOR_URLS_, _EXPENSE_URLS_, _CATEGORY_URLS_, _STATES_, $templateCache) {
+            $rootScope = _$rootScope_;
+            $state = _$state_;
+            $location = _$location_;
+            $injector = _$injector_;
+            $httpBackend = _$httpBackend_;
+            AccountModal = _AccountModal_;
+            $q = _$q_;
+            ENV = _ENV_;
+            CURRENCY_URLS = _CURRENCY_URLS_;
+            COLOR_URLS = _COLOR_URLS_;
+            EXPENSE_URLS = _EXPENSE_URLS_;
+            CATEGORY_URLS = _CATEGORY_URLS_;
+            STATES = _STATES_;
+            URLTo.apiBase(ENV.apiEndpoint);
+
+            // We need add the template entry into the templateCache if we ever specify a templateUrl
+            $templateCache.put('template.html', '');
+
+            $httpBackend.whenGET(URLTo.api(CURRENCY_URLS.allCurrencies)).respond(200, []);
+            $httpBackend.whenGET(URLTo.api(COLOR_URLS.allColors)).respond(200, []);
+            $httpBackend.whenGET(URLTo.api(EXPENSE_URLS.allExpenses)).respond(200, []);
+            $httpBackend.whenGET(URLTo.api(CATEGORY_URLS.allCategories)).respond(200, []);
+
+            AuthServiceMock.logout = jasmine.createSpy('logout');
+            AuthServiceMock.saveAttemptUrl = jasmine.createSpy('saveAttemptUrl');
+        })
+    });
+
+    it('should redirect to expenses page a authenticated user', function () {
+        AuthServiceMock.isAuthenticated = jasmine.createSpy('isAuthenticated').and.returnValue(true);
+
+        UserMock.$new = jasmine.createSpy('$new').and.returnValue({
+            loadFromSession: function () {
+                return {
+                    isInitiated: function () {
+                        return true;
+                    },
+                    isTrialPeriodExpired: function () {
+                        return false;
+                    }
+                };
+            }
+        });
+
+        $state.go(STATES.home);
+        $rootScope.$digest();
+        expect($state.current.abstract).toBe(true);
+
+        $state.go(STATES.account);
+        $rootScope.$digest();
+        expect($state.current.abstract).toBe(true);
+    });
+
+    it('should redirect to non public page to account page', function () {
+        AuthServiceMock.isAuthenticated = jasmine.createSpy('isAuthenticated').and.returnValue(false);
+
+        UserMock.$new = jasmine.createSpy('$new').and.returnValue({
+            loadFromSession: function () {
+                return {
+                    isInitiated: function () {
+                        return true;
+                    },
+                    isTrialPeriodExpired: function () {
+                        return false;
+                    }
+                };
+            }
+        });
+
+        // ---
+        // Non public redirected.
+        // ---
+        expect(AccountModal.isOpen).toBe(false);
+        $state.go(STATES.expenses);
+        $rootScope.$digest();
+        expect($state.current.name).toBe(STATES.account);
+
+        // ---
+        // Public is not redirected.
+        // ---
+        $state.go(STATES.home);
+        $rootScope.$digest();
+        expect($state.current.name).toBe(STATES.home);
+        expect(AccountModal.isOpen).toBe(false);
+    });
+
+    it('should not let user on the setup page once is initiated', function () {
+        AuthServiceMock.isAuthenticated = jasmine.createSpy('isAuthenticated').and.returnValue(true);
+
+        UserMock.$new = jasmine.createSpy('$new').and.returnValue({
+            loadFromSession: function () {
+                return {
+                    isInitiated: function () {
+                        return true;
+                    },
+                    isTrialPeriodExpired: function () {
+                        return false;
+                    }
+                };
+            }
+        });
+
+        // ---
+        // Non public redirected.
+        // ---
+        $state.go("setup");
+        $rootScope.$digest();
+        expect($state.current.abstract).toBe(true);
+    });
+
+    /*it('should not let user on other pages than setup and public pages if is NOT initiated', function () {
+     AuthServiceMock.isAuthenticated = jasmine.createSpy('isAuthenticated').and.returnValue(true);
+
+     UserMock.$new = jasmine.createSpy('$new').and.returnValue({
+     loadFromSession: function () {
+     return {
+     isInitiated: function () {
+     return false;
+     },
+     isTrialPeriodExpired: function () {
+     return false;
+     }
+     };
+     }
+     });
+
+     $state.go(STATES.expenses);
+     $rootScope.$digest();
+     expect($state.current.name).toBe("");
+     })*/
+    ;
+
+});
