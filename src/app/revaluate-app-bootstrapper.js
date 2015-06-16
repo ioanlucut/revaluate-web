3316809c1b36c2b3239dba7a26a2325c0b581c15
I. Lucut <ioan.lucut88@gmail.com>
2015-06-16 20:14:58 +0200

    refactor() - App config calls should be backed up by a cache

--- src/app/revaluate-app-bootstrapper.js
+++ src/app/revaluate-app-bootstrapper.js
@@ -4,13 +4,27 @@ deferredBootstrapper
     .bootstrap({
         element: document.documentElement,
         module: "revaluate",
-        injectorModules: "config",
+        injectorModules: ["config", "angular-cache"],
         resolve: {
-            APP_CONFIG: ['ENV', '$http', function (ENV, $http) {
+            APP_CONFIG: ['ENV', '$http', 'CacheFactory', function (ENV, $http, CacheFactory) {
+                var APP_CACHE_FACTORY_NAME = 'appCache';
+                var APP_CONFIG_RESOURCE_URL = "appconfig/fetchConfig";
+
                 URLTo.apiBase(ENV.apiEndpoint);
 
+                if ( !CacheFactory.get(APP_CACHE_FACTORY_NAME) ) {
+                    CacheFactory
+                        .createCache(APP_CACHE_FACTORY_NAME, {
+                            deleteOnExpire: 'aggressive',
+                            recycleFreq: 60000
+                        });
+                }
+
+                var appCache = CacheFactory
+                    .get(APP_CACHE_FACTORY_NAME);
+
                 return $http
-                    .get(URLTo.api("appconfig/fetchConfig"));
+                    .get(URLTo.api(APP_CONFIG_RESOURCE_URL), { cache: appCache });
             }
             ]
         }
