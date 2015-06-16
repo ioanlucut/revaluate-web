2fd82778b9354f6db09cf3400c6176dbe2c841e2
I. Lucut <ioan.lucut88@gmail.com>
2015-06-16 21:09:21 +0200

    feat() - reenabled insight legend

--- src/app/insight/controllers/insight-controller.js
+++ src/app/insight/controllers/insight-controller.js
@@ -64,7 +64,7 @@ angular
         };
 
         $scope.donutChartOptions = angular.extend({}, $scope.defaultChartOptions);
-        $scope.donutChartOptions.legendTemplate = $templateCache.get("/app/insight/partials/chart/chart.doughnut.legend.template.html");
+        $scope.donutChartOptions.legendTemplate =  "<ul class=\"doughnut__chart\"><% for (var i=0; i<segments.length; i++){%><li class=\"doughnut__chart__legend\"><span class=\"doughnut__chart__legend__color\" style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>";
 
         function formatValue(label) {
             return $filter('currency')(label.value.toString(), $scope.user.model.currency.symbol);
