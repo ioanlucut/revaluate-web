2ac07e2c6978eac6f1bd5371b549c8fbe3632f76
I. Lucut <ioan.lucut88@gmail.com>
2015-06-26 17:57:27 +0200

    feat() - https://github.com/sorinpantis/revaluate-web/issues/97

--- src/app/common/filters/currencys-filter.js
+++ src/app/common/filters/currencys-filter.js
@@ -5,7 +5,7 @@ angular
     .filter('currencys', function ($filter) {
         return function (num, symbol, fractionSize) {
 
-            return $filter('currency')(num, '<span class="currency__symbol">' + symbol + '</span>', fractionSize);
+            return $filter('currency')(num, '', fractionSize) + '<span class="currency__symbol">' + symbol + '</span>';
         };
     }
 );
