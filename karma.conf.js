ea4f9c43f8c39cc55d34b2c9adef87c26e5da8b3
I. Lucut <ioan.lucut88@gmail.com>
2015-07-04 12:03:03 +0200

    fix() - Tests updated.

--- karma.conf.js
+++ karma.conf.js
@@ -8,7 +8,11 @@ module.exports = function (config) {
 
         colors: true,
 
-        frameworks: ['jasmine'],
+        frameworks: ['browserify', 'jasmine'],
+
+        files: [
+            "src/helpers/tests.js"
+        ],
 
         ngHtml2JsPreprocessor: {
             stripPrefix: 'src',
@@ -20,16 +24,24 @@ module.exports = function (config) {
         plugins: [
             'karma-phantomjs-launcher',
             'karma-jasmine',
+            'karma-browserify',
             'karma-ng-html2js-preprocessor'
         ],
 
         preprocessors: {
+            'src/**/*_test.js': ['browserify'],
+            'tests/**/*.js': ['browserify'],
             'src/**/*.html': ['ng-html2js']
         },
 
         exclude: [
             '**/*bootstrapper.js'
-        ]
+        ],
+
+        browserify: {
+            debug: true,
+            paths: ['./node_modules', './src']
+        }
     };
 
     config.set(configuration);
--- src/app/account/account_account_test.js
+++ src/app/account/account_account_test.js
@@ -1,5 +1,10 @@
 'use strict';
 
+// ---
+// Utilities.
+// ---
+var testUtils = require("helpers/tests");
+
 describe('app/account/account', function () {
 
     var $rootScope, $state, $injector, state = 'account';
@@ -9,41 +14,17 @@ describe('app/account/account', function () {
         // ---
         // Load templates.
         // ---
-        module("gulpAngular");
+        angular.mock.module("gulpAngular");
 
         // ---
         // Provide APP_CONFIG.
         // ---
-        module(function ($provide) {
-            $provide.constant('APP_CONFIG', {
-                "SETUP_MIN_CATEGORIES_TO_SELECT": 3,
-                "PREDEFINED_CATEGORIES": ["Bills", "Food"],
-                "CURRENCIES": [{
-                    "currencyCode": "AED",
-                    "displayName": "United Arab Emirates Dirham",
-                    "symbol": "د.إ.‏",
-                    "numericCode": 784,
-                    "fractionSize": 2
-                }],
-                "VERSION": "1.0.0",
-                "MAX_ALLOWED_CATEGORIES": 20,
-                "IMPORT_MIN_CATEGORIES_TO_SELECT": 1,
-                "TRIAL_DAYS": 15,
-                "MIN_ALLOWED_CATEGORIES": 3,
-                "MIN_EXPENSES_TO_ENABLE_BULK_ACTION": 1,
-                "ALL_COLORS": [{ "id": 1, "color": "#DD5440", "colorName": "red", "priority": 1 }, {
-                    "id": 2,
-                    "color": "#E29C45",
-                    "colorName": "orange",
-                    "priority": 2
-                }]
-            });
-        })
+        angular.mock.module(testUtils.mockAppConfig);
 
         // ---
-        // Just inject the module and define dependencies.
+        // Just inject the angular.mock.module and define dependencies.
         // ---
-        module('revaluate', function () {
+        angular.mock.module('revaluate', function () {
         });
 
         inject(function (_$rootScope_, _$state_, _$injector_, $templateCache) {
