85e094b98951589880c6bb9fc7d5616c636c93ab
I. Lucut <ioan.lucut88@gmail.com>
2015-07-09 20:09:44 +0200

    feat() - Change the api requests over SSL using Heroku domain #139

--- src/app/revaluate-app/config/revaluate-app-config.js
+++ src/app/revaluate-app/config/revaluate-app-config.js
@@ -2,7 +2,7 @@ angular.module("config", [])
 
 .constant("ENV", {
 	"name": "development",
-	"apiEndpoint": "http://revaluate-api-dev.herokuapp.com",
+	"apiEndpoint": "https://revaluate-api-dev.herokuapp.com",
 	"mixPanelId": "216177bcdddef0cf2edd1650e63a3449",
 	"intercomAppId": "z2mfyywv",
 	"isProduction": false
