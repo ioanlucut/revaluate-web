a5f99905e02be364aae27555fb29cad49eee537b
I. Lucut <ioan.lucut88@gmail.com>
2015-07-13 21:18:10 +0200

    feat() - Greetings updated

--- src/app/common/constants/greeter-constants.js
+++ src/app/common/constants/greeter-constants.js
@@ -15,14 +15,23 @@ angular
                 "You look great today, {0}!",
                 "Hey {0}! You look great today!",
                 "Hey {0}, you must have had a really good sleep last night, 'cause you look stunning!",
-                "We like you."
+                "We like you.",
+                "Even the darkest night will end and the sun will rise.",
+                "I am feeling healthy and strong today.",
+                "I have all that I need to make this a great day of my life.",
+                "I have all the information I need to solve any challenges that come up today.",
+                "I have the knowledge to make smart decisions for myself today.",
+                "I make the right choices all day using my inner wisdom."
             ]
         },
         DAY: {
             greets: [
                 "Hey {0}! Seems to be a though day, huh ?!",
                 "Hey {0}! Perfect timing to track some expenses. We've been thinking about you.",
-                "Hey {0}! Thanks for stopping by."
+                "Hey {0}! Thanks for stopping by.",
+                "Hey {0}, I feel so nervous with you around.",
+                "{0}, you are amazing just the way you are!",
+                "Leave your worries (and your shoes) at the door."
             ]
         },
         EVENING: {
