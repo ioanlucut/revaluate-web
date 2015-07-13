5c776b9f1320fa1541ba4873338086c9e1994645
I. Lucut <ioan.lucut88@gmail.com>
2015-07-13 21:11:35 +0200

    feat() - Greetings updated

--- src/app/common/constants/greeter-constants.js
+++ src/app/common/constants/greeter-constants.js
@@ -6,19 +6,23 @@ angular
         NIGHT: {
             greets: [
                 "Hey {0}! Can't sleep? Perfect time to write down some expenses.",
-                "Hey {0}! Really nice to see you around."
+                "Hey {0}! Really nice to see you around.",
+                "It is enough for today. Go get some sleep."
             ]
         },
         MORNING: {
             greets: [
                 "You look great today, {0}!",
-                "Hey {0}! You look great today!"
+                "Hey {0}! You look great today!",
+                "Hey {0}, you must have had a really good sleep last night, 'cause you look stunning!",
+                "We like you."
             ]
         },
         DAY: {
             greets: [
                 "Hey {0}! Seems to be a though day, huh ?!",
-                "Hey {0}! Perfect timing to track some expenses. We've been thinking about you"
+                "Hey {0}! Perfect timing to track some expenses. We've been thinking about you.",
+                "Hey {0}! Thanks for stopping by."
             ]
         },
         EVENING: {
