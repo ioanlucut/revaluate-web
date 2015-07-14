b6d57f80bf6671114bf9ee1740c4ad89c35a4431
I. Lucut <ioan.lucut88@gmail.com>
2015-07-14 19:32:22 +0200

    feat() - Bootstrap the front-end for the new types of reports #149

--- src/app/insight/insights.js
+++ src/app/insight/insights.js
@@ -8,9 +8,24 @@ angular
     .config(function ($stateProvider, USER_ACTIVITY_EVENTS) {
 
         $stateProvider
-            .state("insights", {
+
+            // ---
+            // Abstract state - insights.
+            // ---
+            .state({
+                name: "insights",
                 url: "/insights",
-                templateUrl: '/app/insight/partials/insight.html',
+                templateUrl: "/app/insight/partials/insight.abstract.html",
+                abstract: true
+            })
+
+            // ---
+            // Monthly page.
+            // ---
+            .state({
+                name: "insights.monthly",
+                url: "/monthly",
+                templateUrl: "/app/insight/partials/insight.monthly.html",
                 controller: "InsightController",
                 controllerAs: 'vm',
                 resolve: {
@@ -26,8 +41,30 @@ angular
                             .fetchStatistics();
                     }
                 },
-                title: "Insights - Revaluate",
+                title: "Insights monthly - Revaluate",
                 stateEventName: USER_ACTIVITY_EVENTS.insightsPage
             })
+
+            // ---
+            // Overview page.
+            // ---
+            .state({
+                name: "insights.overview",
+                url: "/overview",
+                templateUrl: "/app/insight/partials/insight.overview.html",
+                title: "Insights overview - Revaluate",
+                stateEventName: USER_ACTIVITY_EVENTS.insightsPage
+            })
+
+            // ---
+            // Progress page.
+            // ---
+            .state({
+                name: "insights.progress",
+                url: "/progress",
+                templateUrl: "/app/insight/partials/insight.progress.html",
+                title: "Insights progress - Revaluate",
+                stateEventName: USER_ACTIVITY_EVENTS.insightsPage
+            });
 
     });
