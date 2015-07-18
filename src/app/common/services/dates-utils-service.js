aded27d28dffed13459894a4d8e90128a8f0938a
I. Lucut <ioan.lucut88@gmail.com>
2015-07-18 15:36:29 +0200

    fix() - Fix months interval bug

--- src/app/common/services/dates-utils-service.js
+++ src/app/common/services/dates-utils-service.js
@@ -7,34 +7,14 @@ angular
     .module("revaluate.common")
     .service("DatesUtils", function () {
 
-        this.prepareDate = function (givenDate) {
+        this.fromLastMonthsToNow = function (intervalMonths) {
+            var from = moment().subtract(intervalMonths - 1, "M").startOf('month');
+            var to = moment().add(1, 'month').startOf('month');
 
-            // ---
-            // Minutes.
-            // ---
-            var step = 30;
-
-            // ---
-            // Current values.
-            // ---
-
-            var now = moment();
-            var minute = now.minutes();
-            var hours = now.hours();
-
-            // ---
-            // Compute the date.
-            // ---
-
-            if ( minute >= step ) {
-                minute = 0;
-                hours += 1;
+            return {
+                from: from,
+                to: to
             }
-            else {
-                minute = step;
-            }
-
-            return Date.create(givenDate).set({ hours: hours, minute: minute, second: 0 });
         };
 
     });
