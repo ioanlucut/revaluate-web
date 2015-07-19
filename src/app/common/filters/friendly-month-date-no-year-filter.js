d92c46d42f036ff705f7917dc06b96fb6bca0017
I. Lucut <ioan.lucut88@gmail.com>
2015-07-19 22:17:16 +0200

    fix() - Format months without the year even for the last year #161

---
+++ src/app/common/filters/friendly-month-date-no-year-filter.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular
+    .module("revaluate.common")
+    .filter('friendlyMonthDateNoYear', function () {
+        return function (date) {
+
+            if ( !_.isDate(date) ) {
+                date = moment(new Date(date));
+            }
+
+            return moment(date).format('MMMM');
+        };
+    });
