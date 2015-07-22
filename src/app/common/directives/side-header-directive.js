1415a2df8826c169a63ccbf10170868640d69eb6
I. Lucut <ioan.lucut88@gmail.com>
2015-07-22 20:58:12 +0200

    refactor() - header

---
+++ src/app/common/directives/side-header-directive.js
@@ -0,0 +1,36 @@
+'use strict';
+
+angular
+    .module("revaluate.common")
+    .directive("sideHeader", function ($rootScope, StatesHandler, $state, $timeout) {
+        return {
+            restrict: "AE",
+            templateUrl: "/app/common/partials/side-header-directive.tpl.html",
+            link: function (scope) {
+
+                /**
+                 * Save state to scope
+                 */
+                scope.$state = $state;
+
+                /**
+                 * Reference to the current user.
+                 */
+                scope.currentUser = $rootScope.currentUser;
+
+                /**
+                 * Handles tour page link
+                 */
+                scope.goToTourPage = function () {
+
+                    StatesHandler.goHome(function () {
+                        $timeout(function () {
+                            $rootScope.$broadcast("fullpage-scroll-to", { slideNumber: 2 });
+                        })
+                    });
+                }
+
+            }
+        };
+    });
+
