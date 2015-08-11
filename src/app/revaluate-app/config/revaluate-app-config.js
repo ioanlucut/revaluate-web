3cbe7572de7cfa0d8c92ae2bbcc94e82964f2900
I. Lucut <ioan.lucut88@gmail.com>
2015-08-11 23:49:32 +0300

    feat() - Intercom stuff

--- src/app/revaluate-app/config/revaluate-app-config.js
+++ src/app/revaluate-app/config/revaluate-app-config.js
@@ -1,16 +1,16 @@
 angular.module("config", [])
 
 .constant("ENV", {
-	"name": "local-dev",
+	"name": "development",
 	"apiEndpoint": "https://revaluate-api-dev.herokuapp.com",
 	"mixPanelId": "216177bcdddef0cf2edd1650e63a3449",
 	"intercomAppId": "z2mfyywv",
-	"cacheResetKey": "a2jonsnow",
+	"cacheResetKey": "a1jonsnow",
 	"OAUTH2_CLIENT_IDS": {
-		"FACEBOOK": "933608719993745",
+		"FACEBOOK": "943425629012054",
 		"GOOGLE": "997617697610-sl7dselm1osn67p84ks5vvupbui2nhlb.apps.googleusercontent.com"
 	},
-	"redirectUri": "http://localhost:3000",
+	"redirectUri": "http://dev.revaluate.io",
 	"isProduction": false
 })
 
