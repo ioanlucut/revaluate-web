4effef21a41289e08df5fb4f2fa51e4a28951ce1
I. Lucut <ioan.lucut88@gmail.com>
2015-08-20 20:25:53 +0200

    refactor() - Insights abstract controller

--- src/app/insights/controllers/insights-abstract-controller.js
+++ src/app/insights/controllers/insights-abstract-controller.js
@@ -21,46 +21,42 @@
         vm.monthsPerYearsStatistics = monthsPerYearsStatistics;
 
         // ---
-        // Chart config options.
-        // ---
-        var defaultChartOptions = {
-            scaleLabel: function (label) {
-
-                return formatChartValue(label);
-            },
-
-            multiTooltipTemplate: function (label) {
-
-                return label.datasetLabel + ' ' + formatChartValue(label);
-            },
-
-            tooltipTemplate: function (label) {
-
-                return label.label + ' ' + formatChartValue(label);
-            }
-        };
-
-        /**
-         * Formats chart value
-         */
-        function formatChartValue(price) {
-
-            return $filter('currency')(price.value.toString(), '', vm.user.model.currency.fractionSize) + ' ' + vm.user.model.currency.symbol
-        }
-
-        // ---
         // Specific bar chart options.
         // ---
-        vm.barOptions = angular.extend({}, defaultChartOptions);
+        vm.barOptions = angular.extend({}, getDefaultChartOptions());
 
         // ---
         // Specific donut chart options.
         // ---
         vm
-            .donutChartOptions = angular.extend({}, defaultChartOptions);
+            .donutChartOptions = angular.extend({}, getDefaultChartOptions());
         vm
             .donutChartOptions
             .legendTemplate = '<ul class="doughnut__chart__legend"><% for (var i=0; i<segments.length; i++){%><li class="doughnut__chart__legend__box"><span class="doughnut__chart__legend__box__color" style="background-color:<%=segments[i].fillColor%>"></span><span class="doughnut__chart__legend__box__label"><%if(segments[i].label){%><%=segments[i].label%><%}%></span></li><%}%></ul>';
+
+        // ---
+        // Chart config options.
+        // ---
+        function getDefaultChartOptions() {
+            return {
+                scaleLabel: function (label) {
+                    return formatChartValue(label);
+                },
+
+                multiTooltipTemplate: function (label) {
+                    return label.datasetLabel + ' ' + formatChartValue(label);
+                },
+
+                tooltipTemplate: function (label) {
+                    return label.label + ' ' + formatChartValue(label);
+                }
+            }
+        }
+
+        function formatChartValue(price) {
+
+            return $filter('currency')(price.value.toString(), '', vm.user.model.currency.fractionSize) + ' ' + vm.user.model.currency.symbol
+        }
 
         // ---
         // RESPONSIVE RELATED.
