e45c804d6fc2a1e3d5d668c3a4a76edf946e7851
I. Lucut <ioan.lucut88@gmail.com>
2015-09-10 20:01:01 +0200

    fix() - Labels.. nu le pot orienta altfel :-s

--- src/app/common/filters/friendly-month-day-filter.js
+++ src/app/common/filters/friendly-month-day-filter.js
@@ -10,7 +10,7 @@
                     date = moment(new Date(date));
                 }
 
-                return moment(date).format('ddd Do,');
+                return moment(date).format('D');
             };
         });
 }());
