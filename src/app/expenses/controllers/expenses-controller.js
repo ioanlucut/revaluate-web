a5f305a61bbdfc17b59fa022a57c01783b445e85
sorinpantis <sorinpantis87@gmail.com>
2015-11-03 09:19:15 +0200

    Fix error state position - #312

--- src/app/expenses/controllers/expenses-controller.js
+++ src/app/expenses/controllers/expenses-controller.js
@@ -153,7 +153,7 @@
             vm.temporaryExpenses.push(args.expense);
 
             $scope.$emit('trackEvent', USER_ACTIVITY_EVENTS.expenseCreated);
-            $scope.$emit(ALERTS_EVENTS.SUCCESS, 'Saved');
+            $scope.$emit(ALERTS_EVENTS.SUCCESS, 'Saved expense.');
         });
 
         /**
@@ -170,7 +170,7 @@
             }
             vm.temporaryExpenses.push(args.expense);
 
-            $scope.$emit(ALERTS_EVENTS.SUCCESS, 'Updated');
+            $scope.$emit(ALERTS_EVENTS.SUCCESS, 'Updated expense.');
             $scope.$emit('trackEvent', USER_ACTIVITY_EVENTS.expenseUpdated);
         });
 
@@ -182,7 +182,7 @@
                 removeBulkExpenses(args.expenses);
             }
 
-            $scope.$emit(ALERTS_EVENTS.SUCCESS, 'Deleted');
+            $scope.$emit(ALERTS_EVENTS.SUCCESS, 'Deleted expense.');
             $scope.$emit('trackEvent', USER_ACTIVITY_EVENTS.expenseDeleted);
         });
 
