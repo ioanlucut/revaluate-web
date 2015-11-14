612d5253d3563ae1cb276d164d1d044ae62e6f96
sorinpantis <sorinpantis87@gmail.com>
2015-11-14 17:10:43 +0200

    Some tests.

--- e2e/home.po.js
+++ e2e/home.po.js
@@ -4,7 +4,9 @@
     var HomePage = function () {
         this.mainTitle = element(by.css('.home__title'));
         this.mainDescription = element(by.css('.home__description'));
-        this.signUpButton = element(by.css('.home__section__start__btn'));
+        this.logInButton = element(by.css('.header-public__navigation__loginbtn'));
+        this.signUpButton = element(by.css('.hgiome__section__start__btn'));
+        this.accountGreeting = element(by.css('.account__title'));
     };
 
     module.exports = new HomePage();
--- e2e/home.spec.js
+++ e2e/home.spec.js
@@ -15,5 +15,15 @@
             expect(page.mainDescription.getText()).toBe('Simplify your personal finance management');
             expect(page.mainTitle.getText()).toBe('Change the way you spend your money');
         });
+
+        it('should open the account modal', function () {
+            page.logInButton.click();
+            expect(page.accountGreeting.getText()).toContain('Welcome!');
+        });
+
+        it('should open the account modal', function () {
+            page.signUpButton.click();
+            expect(page.accountGreeting.getText()).toContain('Let\'s get you started!');
+        });
     });
 }());
