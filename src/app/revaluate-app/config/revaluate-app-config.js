360f2be8ad524462bfcf28870d61b472e3d07b68
I. Lucut <ioan.lucut88@gmail.com>
2015-12-05 17:52:22 +0100

    feat(AWS) - DevOps work :/.

--- src/app/revaluate-app/config/revaluate-app-config.js
+++ src/app/revaluate-app/config/revaluate-app-config.js
@@ -1,20 +1,29 @@
 angular.module("config", [])
 
 .constant("ENV", {
-	"name": "local-dev",
+	"name": "development",
 	"apiEndpoint": "https://revaluate-api-dev.herokuapp.com",
 	"mixPanelId": "216177bcdddef0cf2edd1650e63a3449",
 	"intercomAppId": "z2mfyywv",
-	"cacheResetKey": "a2jonsnow",
+	"cacheResetKey": "a1jonsnow",
 	"OAUTH2_CLIENT_IDS": {
-		"FACEBOOK": "933608719993745",
+		"FACEBOOK": "943425629012054",
 		"GOOGLE": "997617697610-sl7dselm1osn67p84ks5vvupbui2nhlb.apps.googleusercontent.com",
 		"SLACK": "2151987168.10687444405"
 	},
-	"redirectUri": "http://localhost:3000",
-	"frontEndUri": "http://localhost:3000",
+	"redirectUri": "http://dev.revaluate.io",
+	"frontEndUri": "http://dev.revaluate.io",
 	"isProduction": false,
-	"isMaintenanceMode": false
+	"isMaintenanceMode": false,
+	"AWS": {
+		"params": {
+			"Bucket": "revaluate-web-prod"
+		},
+		"accessKeyId": "AKIAJIPPTKHKJD2XFEBA",
+		"secretAccessKey": "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",
+		"distributionId": "E20NR4VDPE1BSZ",
+		"region": "us-west-2"
+	}
 })
 
 ;
