1526a49dde448a6378203ae81c3ef1f403dd41c2
I. Lucut <ioan.lucut88@gmail.com>
2015-12-06 14:14:39 +0100

    fix() - Move everything to /assets

--- src/app/revaluate-app/config/revaluate-app-config.js
+++ src/app/revaluate-app/config/revaluate-app-config.js
@@ -1,19 +1,29 @@
 angular.module("config", [])
 
 .constant("ENV", {
-	"name": "local-dev",
-	"apiEndpoint": "https://revaluate-api-dev.herokuapp.com",
-	"mixPanelId": "216177bcdddef0cf2edd1650e63a3449",
-	"intercomAppId": "z2mfyywv",
-	"cacheResetKey": "a2jonsnow",
+	"name": "production",
+	"apiEndpoint": "https://revaluate-api-prod.herokuapp.com",
+	"mixPanelId": "1e6d81ba283c1393b2dee527e165013f",
+	"intercomAppId": "qy0w35xm",
+	"cacheResetKey": "a3jonsnow",
 	"OAUTH2_CLIENT_IDS": {
-		"FACEBOOK": "933608719993745",
+		"FACEBOOK": "933591903328760",
 		"GOOGLE": "997617697610-sl7dselm1osn67p84ks5vvupbui2nhlb.apps.googleusercontent.com",
 		"SLACK": "2151987168.10687444405"
 	},
-	"redirectUri": "http://localhost:3000",
-	"isProduction": false,
-	"isMaintenanceMode": false
+	"redirectUri": "https://www.revaluate.io",
+	"isProduction": true,
+	"isMaintenanceMode": false,
+	"AWS": {
+		"params": {
+			"Bucket": "revaluate-web-prod"
+		},
+		"accessKeyId": "AKIAJIPPTKHKJD2XFEBA",
+		"secretAccessKey": "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",
+		"distributionId": "E20NR4VDPE1BSZ",
+		"region": "us-west-2"
+	},
+	"CDN_DOMAIN": "d2l41wngd03v97.cloudfront.net"
 })
 
 ;
