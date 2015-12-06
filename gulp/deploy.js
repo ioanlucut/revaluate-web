965267acdc86afe6409e284affdabaf68c6cc097
I. Lucut <ioan.lucut88@gmail.com>
2015-12-06 14:32:29 +0100

    fix() - Update build and deploy scripts

--- gulp/deploy.js
+++ gulp/deploy.js
@@ -1,8 +1,12 @@
 'use strict';
 
 var gulp = require('gulp'),
+    $ = require('gulp-load-plugins')({
+        pattern: ['gulp-*', 'main-bower-files', 'uglify-save-license', 'del']
+    }),
     cloudfront = require("gulp-cloudfront"),
     RevAll = require('gulp-rev-all'),
+    revAll = new RevAll(),
     awspublish = require('gulp-awspublish'),
     _ = require('lodash'),
     argv = require('yargs').argv,
@@ -17,15 +21,15 @@ var gulp = require('gulp'),
             'Cache-Control': 'max-age=315360000, no-transform, public'
         };
 
-        return gulp.src('dist/**/*.*')
-            .pipe(new RevAll().revision())
+        return gulp.src('dist/**')
+            .pipe(revAll.revision())
             // gzip, Set Content-Encoding headers and add .gz extension
             .pipe(awspublish.gzip())
             // publisher will add Content-Length, Content-Type and headers specified above
             // If not specified it will set x-amz-acl to public-read by default
             .pipe(publisher.publish(headers))
             // create a cache file to speed up consecutive uploads
-            /*.pipe(publisher.sync())*/
+            //.pipe(publisher.sync())
             .pipe(publisher.cache())
             // print upload updates to console
             .pipe(awspublish.reporter())
