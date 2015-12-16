b7707f3388b8ba4991747f086721f647a563c0ae
I. Lucut <ioan.lucut88@gmail.com>
2015-12-16 21:06:04 +0100

    update(angular-chartjs) - ups, forgot this code.

--- src/app/revaluate-app/revaluate-app.js
+++ src/app/revaluate-app/revaluate-app.js
@@ -167,35 +167,7 @@
                 showTooltips: true,
 
                 // Function - Determines whether to execute the customTooltips function instead of drawing the built in tooltips (See [Advanced - External Tooltips](#advanced-usage-custom-tooltips))
-                customTooltips: function (tooltip) {
-                    var tooltipEl = $('#chartjs-tooltip');
-                    if ( !tooltip ) {
-                        tooltipEl.css({
-                            opacity: 0
-                        });
-                        return;
-                    }
-                    tooltipEl.removeClass('above below');
-                    tooltipEl.addClass(tooltip.yAlign);
-                    var innerHtml = '';
-                    for ( var i = tooltip.labels.length - 1; i >= 0; i-- ) {
-                        innerHtml += [
-                            '<div class="chartjs-tooltip-section">',
-                            '	<span class="chartjs-tooltip-key" style="background-color:' + tooltip.legendColors[i].fill + '"></span>',
-                            '	<span class="chartjs-tooltip-value">' + tooltip.labels[i] + '</span>',
-                            '</div>'
-                        ].join('');
-                    }
-                    tooltipEl.html(innerHtml);
-                    tooltipEl.css({
-                        opacity: 1,
-                        left: tooltip.chart.canvas.offsetLeft + tooltip.x + 'px',
-                        top: tooltip.chart.canvas.offsetTop + tooltip.y + 'px',
-                        fontFamily: tooltip.fontFamily,
-                        fontSize: tooltip.fontSize,
-                        fontStyle: tooltip.fontStyle,
-                    });
-                },
+                customTooltips: false,
 
                 // Array - Array of string names to attach tooltip events
                 tooltipEvents: ['mousemove', 'touchstart', 'touchmove'],
