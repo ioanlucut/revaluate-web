25c244ab31cc500b79b63a01613efe425ddc65c0
I. Lucut <ioan.lucut88@gmail.com>
2016-03-06 17:55:45 +0100

    lebab(js): lebabelize files
    es5->es6

--- utils/es5_structure_transform.js
+++ utils/es5_structure_transform.js
@@ -1,10 +1,10 @@
 'use strict';
 
-var dir = require('node-dir');
-var fs = require('fs');
+let dir = require('node-dir');
+let fs = require('fs');
 
 // ---
-// Clear temp file.
+// Replace & clear temp file.
 // ---
 
 function replaceFileWith(toAppend) {
@@ -18,16 +18,22 @@ function clearTempFile() {
 }
 
 // ---
-// On every subdir, iterate over files.
+// Map of the angular module declarations.
 // ---
-
 let map = new Map();
+
+// ---
+// Map of the imports from angular module declarations.
+// ---
 let imports = new Map();
 
+// ---
+// On every subdir, iterate over files.
+// ---
 const BASE_PATH = 'src/app/components/insights';
 
 dir.files('./' + BASE_PATH, function (err, files) {
-  if ( err ) throw err;
+  if (err) throw err;
 
   files = files.filter(function (file) {
     return file.indexOf('.js') > -1;
@@ -35,7 +41,7 @@ dir.files('./' + BASE_PATH, function (err, files) {
 
   //we have an array of files now, so now we'll iterate that array
   files.forEach(function (path) {
-    var content = fs.readFileSync(path);
+    let content = fs.readFileSync(path);
     const importNameRegexExp = /(\/(?=[^/]*$))(.*js)/g;
     const importName = importNameRegexExp.exec(path)[2];
     let importNameWithoutExtension = importName.replace('.js', '');
@@ -43,54 +49,60 @@ dir.files('./' + BASE_PATH, function (err, files) {
 
     const contentRegex = /(angular)(.*[\s\S]*)(module.*)(.*[\s\S]*)((provider|directive|factory|filter|service|value|constant|controller)\('\w+', )(.*[\s\S]*)(\);)/g;
     const contentRegexResult = contentRegex.exec(content);
-    if ( !contentRegexResult ) {
+    if (!contentRegexResult) {
       return;
     }
 
     const module = contentRegexResult[3];
     const declarationModuleValue = contentRegexResult[5] + importNameWithoutExtension + ')';
-    if ( map.get(module) == null ) {
+    if (map.get(module) == null) {
       map.set(module, []);
     }
+
     map.get(module).push(declarationModuleValue);
 
-    if ( imports.get(module) == null ) {
+    if (imports.get(module) == null) {
       imports.set(module, []);
     }
-    imports.get(module).push(importValue);
+
+    imports
+      .get(module)
+      .push(importValue);
 
     console.log('---------');
-    var result = content.toString().replace(contentRegex, '$7');
-    /* if ( result.indexOf('export default') > -1 ) {
-     result = 'export default ' + result.replace('export default', '').trim();
-     }*/
+
+    let result = content.toString().replace(contentRegex, '$7');
+  /*  if (result.indexOf('export default') > -1) {
+      result = 'export default ' + result.replace('export default', '').trim();
+    }*/
+
     console.log(result);
     console.log('---------end');
 
     fs.writeFile(path, result, 'utf8', function (err) {
-      if ( err ) return console.log(err);
+      if (err) return console.log(err);
     });
   });
 
   clearTempFile();
 
-  for ( let item of imports ) {
+  for (let item of imports) {
     replaceFileWith('--------------------------' + '\r\n');
     replaceFileWith(item[0] + '\r\n');
 
-    for ( let itm of item[1] ) {
-      replaceFileWith(itm.replace('.js', '') + '\r\n')
+    for (let itm of item[1]) {
+      replaceFileWith(itm.replace('.js', '') + '\r\n');
     }
   }
 
-  for ( let item of map ) {
+  for (let item of map) {
     replaceFileWith('--------------------------' + '\r\n');
     replaceFileWith(item[0] + '\r\n');
 
-    for ( let itm of item[1] ) {
+    for (let itm of item[1]) {
       replaceFileWith('.' + itm + '\r\n');
     }
   }
 
-  console.log('DONE!!!!!!!');
-})
+  console.log('done');
+});
